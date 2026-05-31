08bdaafbdf0a9630f1d403f9c05b0e3e7f86634f
zzha0678 <zzha0678@student.monash.edu>
2026-05-31 23:39:21 +1000

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,42 @@
 import json
 import boto3
+import os
 
 s3 = boto3.client("s3")
+lambda_client = boto3.client("lambda")
+
+PART2_FUNCTION_NAME = os.environ.get("PART2_FUNCTION_NAME")
+
+
+def invoke_part2(bucket, key):
+    if not PART2_FUNCTION_NAME:
+        print("PART2_FUNCTION_NAME is not set, skip invoking Part2")
+        return
+
+    payload = {
+        "Records": [
+            {
+                "s3": {
+                    "bucket": {
+                        "name": bucket
+                    },
+                    "object": {
+                        "key": key
+                    }
+                }
+            }
+        ]
+    }
+
+    response = lambda_client.invoke(
+        FunctionName=PART2_FUNCTION_NAME,
+        InvocationType="Event",
+        Payload=json.dumps(payload).encode("utf-8")
+    )
+
+    print("Part2 Lambda invoked:")
+    print(response)
+
 
 def lambda_handler(event, context):
     print("Event received:")
@@ -21,10 +56,12 @@ def lambda_handler(event, context):
 
         print("File loaded successfully")
 
+        invoke_part2(bucket, key)
+
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "S3 upload event processed",
+                "message": "S3 upload event processed and Part2 invoked",
                 "bucket": bucket,
                 "key": key
             })
